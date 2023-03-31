b37b44351363d40f7a8e31757d2b755cd8b271a1
Liebus <sannija.liebus@lvt.sk.lv>
2023-03-31 09:21:05 +0300

    Toggle elements

---
+++ Assets/Scripts/AtteluParadisana.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtteluParadisana : MonoBehaviour {
+	public GameObject bean, lacis, tante, masina;
+
+	public void BeanAtt(bool v){
+		bean.SetActive(v); //checkmark
+	}
+	public void GrannyAtt(bool v){
+		tante.SetActive(v); //checkmark
+	}
+	public void CarAtt(bool v){
+		masina.SetActive(v); //checkmark
+	}
+	public void TeddyAtt(bool v){
+		lacis.SetActive(v); //checkmark
+	}
+}
