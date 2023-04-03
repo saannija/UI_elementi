3cc930f807aaa05404fe0cd8a66273c94d699adf
Liebus <sannija.liebus@sk.lvt.lv>
2023-04-03 15:12:18 +0300

    Pabeigts toggle

--- Assets/Scripts/AtteluParadisana.cs
+++ Assets/Scripts/AtteluParadisana.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AtteluParadisana : MonoBehaviour {
-	public GameObject bean, lacis, tante, masina;
+	public GameObject bean, lacis, tante, masina, paK, paL;
 
 	public void BeanAtt(bool v){
 		bean.SetActive(v); //checkmark
-	}
+		paK.GetComponent<Toggle>().interactable = v;
+        paL.GetComponent<Toggle>().interactable = v;
+    }
 	public void GrannyAtt(bool v){
 		tante.SetActive(v); //checkmark
 	}
@@ -17,4 +20,12 @@ public class AtteluParadisana : MonoBehaviour {
 	public void TeddyAtt(bool v){
 		lacis.SetActive(v); //checkmark
 	}
+	public void PaKreisiBean()
+	{
+		bean.transform.localScale = new Vector2(1, 1);
+	}
+    public void PaLabiBean()
+    {
+        bean.transform.localScale = new Vector2(-1, 1);
+    }
 }
